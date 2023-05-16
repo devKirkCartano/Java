5f745434f65c6c069a02f3d52338509c89d3cb05
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-05-16 22:44:31 +0800

    initial commit

---
+++ src/Basics/Variables.java
@@ -0,0 +1,21 @@
+package Basics;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+
+        myFirstNumber = 10;
+        System.out.println(myFirstNumber);
+
+        myFirstNumber = 1000;
+        System.out.println(myFirstNumber);
+
+        myFirstNumber = 10 + 5;
+        System.out.println(myFirstNumber);
+
+        myFirstNumber = 10 + 5 + (2 * 10);
+        System.out.println(myFirstNumber);
+    }
+}
